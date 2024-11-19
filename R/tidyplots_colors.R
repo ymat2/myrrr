98f8f3f15322b956a39e674af7873586833ae0fb
ymat2 <ymat2@users.noreply.github.com>
2024-11-19 16:48:15 +0900

    :bug: Remove example and Add descriptions

--- R/tidyplots_colors.R
+++ R/tidyplots_colors.R
@@ -1,10 +1,5 @@
 #' Custom color schemes of tidyplots
 #'
-#' @examples
-#' (energy %>%
-#'    tidyplot(year, power, color = energy_source) %>%
-#'    add_barstack_absolute() %>%
-#'    adjust_colors(new_colors = colors_discrete_set1)
 #' @rdname tidyplots_colors
 #' @export
 
@@ -13,6 +8,8 @@ colors_discrete_r3 = tidyplots::new_color_scheme(
   name = "colors_discrete_r3"
 )
 
+#' @description
+#' Custom color scheme using R4 palette
 #' @rdname tidyplots_colors
 #' @export
 colors_discrete_r4 = tidyplots::new_color_scheme(
@@ -20,6 +17,8 @@ colors_discrete_r4 = tidyplots::new_color_scheme(
   name = "colors_discrete_r4"
 )
 
+#' @description
+#' Custom color scheme using Accent palette
 #' @rdname tidyplots_colors
 #' @export
 colors_discrete_accent = tidyplots::new_color_scheme(
@@ -27,6 +26,8 @@ colors_discrete_accent = tidyplots::new_color_scheme(
   name = "colors_discrete_accent"
 )
 
+#' @description
+#' Custom color scheme using Dark 2 palette
 #' @rdname tidyplots_colors
 #' @export
 colors_discrete_dark2 = tidyplots::new_color_scheme(
@@ -34,6 +35,8 @@ colors_discrete_dark2 = tidyplots::new_color_scheme(
   name = "colors_discrete_dark2"
 )
 
+#' @description
+#' Custom color scheme using Paired palette
 #' @rdname tidyplots_colors
 #' @export
 colors_discrete_paired = tidyplots::new_color_scheme(
@@ -41,6 +44,8 @@ colors_discrete_paired = tidyplots::new_color_scheme(
   name = "colors_discrete_paired"
 )
 
+#' @description
+#' Custom color scheme using Set 1 palette
 #' @rdname tidyplots_colors
 #' @export
 colors_discrete_set1 = tidyplots::new_color_scheme(
@@ -48,6 +53,8 @@ colors_discrete_set1 = tidyplots::new_color_scheme(
   name = "colors_discrete_set1"
 )
 
+#' @description
+#' Custom color scheme using Set 2 palette
 #' @rdname tidyplots_colors
 #' @export
 colors_discrete_set2 = tidyplots::new_color_scheme(
@@ -55,6 +62,8 @@ colors_discrete_set2 = tidyplots::new_color_scheme(
   name = "colors_discrete_set2"
 )
 
+#' @description
+#' Custom color scheme using Set 3 palette
 #' @rdname tidyplots_colors
 #' @export
 colors_discrete_set3 = tidyplots::new_color_scheme(
