427a493c01dfd8b956a8b11491f6fb3126bb9e2b
ymat2 <ymat2@users.noreply.github.com>
2025-02-06 14:27:03 +0900

    :sparkles: Return merged dataframe

--- R/annotate.R
+++ R/annotate.R
@@ -1,16 +1,16 @@
 #' Annotate gene name to bed style dataframe
 #'
 #' @param df A data.frame. with bed style
-#' @param seqnames Colname passed to seqnames.field
-#' @param start Colname passed to start.field
-#' @param end Colname passed to end.field
-#' @param colname EctraColname to be printed
+#' @param seqnames Colname passed to seqnames.field (e.g. `CHROM`)
+#' @param start Colname passed to start.field (e.g. `BIN_START`)
+#' @param end Colname passed to end.field (e.g. `BIN_END`)
+#' @param ... Other arguments passed on to [GenomicRanges::makeGRangesFromDataFrame()]
 #' @importFrom rlang .data
+#' @importFrom rlang :=
 #' @rdname annotate
 #' @export
 
-
-annotate = function(df, seqnames, start, end, colname = NULL) {
+annotate = function(df, seqnames, start, end, ...) {
   gr = GenomicRanges::makeGRangesFromDataFrame(
     df,
     keep.extra.columns = TRUE,
@@ -23,43 +23,31 @@ annotate = function(df, seqnames, start, end, colname = NULL) {
   gff_path = system.file("extdata", "GRCg7b.gff", package = "myrrr")
   gff = rtracklayer::import(gff_path, format = "gff")
 
-  df_merge = IRanges::mergeByOverlaps(
+  df_overlap = IRanges::mergeByOverlaps(
       gr,
       gff,
       maxgap = -1L,
       minoverlap = 0L,
       type = "any",
       select = "all"
-    ) |> dplyr::as_tibble()
+    ) |>
+    dplyr::as_tibble() |>
+    dplyr::select(
+      .data$gr.seqnames,
+      .data$gr.start,
+      .data$gr.end,
+      .data$gene
+    ) |>
+    dplyr::rename(
+      !!seqnames := .data$gr.seqnames,
+      !!start := .data$gr.start,
+      !!end := .data$gr.end
+    )
 
-  if (is.null(colname)) {
-    df_merge = dplyr::select(
-      df_merge,
-      .data$gr.seqnames,
-      .data$gr.start,
-      .data$gr.end,
-      .data$gene
-    ) |>
-      dplyr::rename(
-        seqnames = .data$gr.seqnames,
-        start = .data$gr.start,
-        end = .data$gr.end
-    )
-  } else {
-    df_merge = dplyr::select(
-      df_merge,
-      .data$gr.seqnames,
-      .data$gr.start,
-      .data$gr.end,
-      !!dplyr::sym(colname),
-      .data$gene
-    ) |>
-      dplyr::rename(
-        seqnames = .data$gr.seqnames,
-        start = .data$gr.start,
-        end = .data$gr.end
-    )
-  }
+  df_merge = df |> dplyr::left_join(
+    df_overlap,
+    by = tidyselect::all_of(c(seqnames, start, end))
+  )
 
   return(df_merge)
 }
